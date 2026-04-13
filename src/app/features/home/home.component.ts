86bced7c5000611bdd83fdb859665e553f2dba70
Basma Youssry <basmayoussry17@gmail.com>
2026-04-13 16:52:37 +0200

    created solutions section, created principles section, changing on button in services section

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./components/hero/hero.component";
 import { ServiceComponent } from "./components/service/service.component";
+import { SolutionsComponent } from './components/solutions/solutions.component';
+import { PrinciplesComponent } from './components/principles/principles.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ServiceComponent],
+  imports: [ServiceComponent, SolutionsComponent, PrinciplesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
