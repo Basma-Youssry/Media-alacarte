60b9e7c5a2c07ef40d1d5c9f91bf2dd97e0d13a3
Basma Youssry <basmayoussry17@gmail.com>
2026-04-13 18:20:21 +0200

    created reservation section, some changes

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,10 +3,11 @@ import { HeroComponent } from "./components/hero/hero.component";
 import { ServiceComponent } from "./components/service/service.component";
 import { SolutionsComponent } from './components/solutions/solutions.component';
 import { PrinciplesComponent } from './components/principles/principles.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ServiceComponent, SolutionsComponent, PrinciplesComponent],
+  imports: [ServiceComponent, SolutionsComponent, PrinciplesComponent, ReservationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
